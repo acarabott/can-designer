9d3377e1b3484d438ad20e02c302a62e68d36293
Arthur Carabott <arthurc@gmail.com>
2017-05-13 16:46:38 +0100

    properties with links working

--- js/designer.js
+++ js/designer.js
@@ -6,9 +6,10 @@ const svg = d3.select('body').append('svg')
   .attr('height', height);
 
 const simulation = d3.forceSimulation()
-  .force('link', d3.forceLink().id(d => d.id))
+  .force('link', d3.forceLink().id(d => d.id).distance(d => 80))
   .force('charge', d3.forceManyBody())
-  .force('center', d3.forceCenter(width / 2 , height / 2));
+  .force('center', d3.forceCenter(width / 2 , height / 2))
+  .force('collide', d3.forceCollide().radius(d => d.radius));
 
 let g_graph;
 function render (graph) {
@@ -29,6 +30,8 @@ function render (graph) {
     d.fy = null;
   }
 
+  graph.links = [];
+
   // set up objects
   graph.nodes.forEach(n => {
     n.userEnabled = false;
@@ -37,7 +40,20 @@ function render (graph) {
     n.disable = () => n.userEnabled = false;
     n.toggle = () => n.userEnabled ? n.disable() : n.enable();
 
+    n.properties.forEach(prop => graph.links.push({
+      source: n.id,
+      target: prop
+    }));
+
     Object.defineProperties(n, {
+      visible: {
+        get: () => {
+          if (['1', '2'].includes(n.type)) { return true; }
+          return graph.nodes
+            .filter(node => node.properties.includes(n.id))
+            .some(node => node.enabled);
+        }
+      },
       enabled: {
         get: () => {
           return n.userEnabled || n.enabledBy.some(ids => {
@@ -55,6 +71,17 @@ function render (graph) {
               .every(node => node.enabled);
           });
         },
+      },
+      radius: {
+        get: () => {
+          const radii = {
+            1: 50,
+            2: 40,
+            property: 20
+          };
+
+          return radii[n.type];
+        }
       }
     });
   });
@@ -70,7 +97,13 @@ function render (graph) {
         .on('drag', dragged)
         .on('end', dragended));
 
-  function updateCircles() {
+  const link = svg.append('g')
+      .attr('class', 'links')
+    .selectAll('line')
+    .data(graph.links)
+    .enter().append('line');
+
+  function render() {
     const getAlpha = n => n.enabled ? 1.0 : n.disabled ? 0.1 : 0.5;
     const getColor = n => {
       const colors = {
@@ -80,9 +113,11 @@ function render (graph) {
         green: [43, 212, 156],
         grey: [89, 125, 148],
       };
-      const lookup = { 1: 'blue', 2: 'green', 3: 'orange', 4: 'grey', 5: 'red' };
+      const lookup = { 1: 'blue', 2: 'green', property: 'orange', 4: 'grey', 5: 'red' };
       return colors[lookup[n.type]].join(',');
     };
+
+    node.attr('display', n => n.visible ? '' : 'none');
 
     node.selectAll('circle')
       .attr('fill', n => `rgba(${getColor(n)}, ${getAlpha(n)})`);
@@ -93,16 +128,17 @@ function render (graph) {
     node.selectAll('.ring')
       .attr('fill', 'none')
       .attr('stroke', n => `rgba(${getColor(n)}, ${n.userEnabled ? 1.0 : 0.0})`);
+
+    link.attr('display', l => l.source.enabled ? '' : 'none');
+    link.attr('stroke-width', 2);
   }
 
   node.append('circle')
-    .attr('r', 40);
+    .attr('r', d => d.radius);
 
   node.append('circle')
-    .attr('r', 50)
+    .attr('r', d => d.radius * 1.1)
     .classed('ring', true);
-
-  updateCircles();
 
   node.append('text')
     .attr('dx', 12)
@@ -112,22 +148,29 @@ function render (graph) {
   node.on('click', d => {
     if (d.disabled) { return; }
     d.toggle();
-    updateCircles();
+    render();
   });
-
 
   function ticked() {
     node.attr('transform', d => `translate(${d.x}, ${d.y})`);
+
+    link
+      .attr('x1', d => d.source.x)
+      .attr('y1', d => d.source.y)
+      .attr('x2', d => d.target.x)
+      .attr('y2', d => d.target.y);
+
+    // link.distance(d => 1000);
   }
 
   simulation
     .nodes(graph.nodes)
     .on('tick', ticked);
 
-  // simulation.force('link')
-  //   .links(graph.links);
+  simulation.force('link')
+    .links(graph.links);
 
-
+  render();
 }
 
 d3.json('data/numbers.json', (error, graph) => {
